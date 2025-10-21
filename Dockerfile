7224f7d79fb76f99e76c3107ebc452f77c088b25
hdabhi-calidad <harsh.dabhi@caliadinfotech.com>
2025-10-21 19:01:32 +0530

    [UPDT] DOCKER: Update Dockerfile and entrypoint script to use port 8001 for Gunicorn

--- Dockerfile
+++ Dockerfile
@@ -23,4 +23,4 @@ RUN chmod +x /app/entrypoint.sh
 
 EXPOSE 8001
 
-CMD ["python3", "manage.py", "runserver", "0.0.0.0:8001"]
+ENTRYPOINT ["sh", "./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ echo "Waiting for database to be ready..."
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-python3 manage.py createhorillauser --first_name admin --last_name admin --username admin --password admin --email admin@example.com --phone 1234567890
-gunicorn --bind 0.0.0.0:8000 horilla.wsgi:application
+python3 manage.py createhorillauser --first_name Samay --last_name Thakkar --username samay.thakkar --password horilla@123* --email samay.thakkar@calidadinfotech.com --phone 1234567890
+gunicorn --bind 0.0.0.0:8001 horilla.wsgi:application
